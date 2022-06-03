798563ce91c0e065d065d5b0e2678f18d8baebc9
Özkan Yanikbas <oezkanyanikbas@gmail.com>
2022-06-03 20:17:13 +0200

    Add Storyblok Helper StoryblokImage

---
+++ components/storyblok/helper/StoryblokImage.vue
@@ -0,0 +1,16 @@
+<template>
+  <img
+    v-if="image && image.filename"
+    :src="image.filename"
+    :alt="image.alt || null"
+  />
+</template>
+
+<script setup>
+  defineProps({
+    image: {
+      type: Object,
+      required: true
+    }
+  });
+</script>
