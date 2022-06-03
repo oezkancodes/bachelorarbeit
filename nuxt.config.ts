87c70741f93b79274f89542a203b7470c5488ff7
Özkan Yanikbas <oezkanyanikbas@gmail.com>
2022-06-03 19:21:04 +0200

    Add ESLint & Prettier

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,9 @@
-import { defineNuxtConfig } from 'nuxt'
+import { defineNuxtConfig } from 'nuxt';
+import eslintPlugin from 'vite-plugin-eslint';
 
 // https://v3.nuxtjs.org/api/configuration/nuxt.config
 export default defineNuxtConfig({
-
-})
+  vite: {
+    plugins: [eslintPlugin()]
+  }
+});
