b771a5f449a09d4c4ffe67dcee3fed8ae0a7d657
Özkan Yanikbas <oezkanyanikbas@gmail.com>
2022-06-03 22:28:02 +0200

    Change Nuxt target to static

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,6 +3,7 @@ import eslintPlugin from 'vite-plugin-eslint';
 
 // https://v3.nuxtjs.org/api/configuration/nuxt.config
 export default defineNuxtConfig({
+  target: 'static',
   vite: {
     plugins: [eslintPlugin()]
   },
