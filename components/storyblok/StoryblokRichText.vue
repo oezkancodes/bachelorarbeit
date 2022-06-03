2501b4ce61d65ffd6faad06dfc3437f2cf3ac417
Özkan Yanikbas <oezkanyanikbas@gmail.com>
2022-06-03 20:15:29 +0200

    Add Storyblok Helper StoryblokRichText

---
+++ components/storyblok/StoryblokRichText.vue
@@ -0,0 +1,18 @@
+<template>
+  <div v-if="text" v-html="richText" />
+</template>
+
+<script setup>
+  const props = defineProps({
+    text: {
+      type: Object,
+      required: true
+    }
+  });
+
+  const storyapi = useStoryblokApi();
+
+  const richText = computed(() => {
+    return storyapi.richTextResolver.render(props.text);
+  });
+</script>
