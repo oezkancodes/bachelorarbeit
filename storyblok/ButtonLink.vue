1db2b828d95c742d4d6d69823f04904239cbac95
Özkan Yanikbas <oezkanyanikbas@gmail.com>
2022-06-03 20:36:53 +0200

    Add ButtonLink Blok

---
+++ storyblok/ButtonLink.vue
@@ -0,0 +1,34 @@
+<template>
+  <NuxtLink
+    v-if="blok"
+    class="inline-flex cursor-pointer items-center rounded-md border border-transparent px-6 py-3 text-base font-medium focus:outline-none focus:ring-2 focus:ring-offset-2"
+    :class="[
+      {
+        'bg-indigo-600 text-white shadow-sm hover:bg-indigo-700 focus:ring-indigo-500':
+          blok.type === 'primary'
+      },
+      {
+        'bg-indigo-100 text-indigo-700  hover:bg-indigo-200 focus:ring-indigo-500':
+          blok.type === 'secondary'
+      }
+    ]"
+    :to="url"
+    @click="$emit('click')"
+  >
+    {{ blok.label }}
+  </NuxtLink>
+</template>
+
+<script setup>
+  const props = defineProps({
+    blok: {
+      type: Object,
+      required: true
+    }
+  });
+
+  const url =
+    props.blok.link.linktype === 'story'
+      ? '/' + linkResolver(props.blok.link.cached_url)
+      : props.blok.link.cached_url;
+</script>
