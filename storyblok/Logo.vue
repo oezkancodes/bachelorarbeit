bb8922da486d31aa4142aaefe32df611480f5ee3
Özkan Yanikbas <oezkanyanikbas@gmail.com>
2022-06-03 21:38:56 +0200

    Add Logo Blok

---
+++ storyblok/Logo.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="h-12 flex items-center justify-center">
+    <StoryblokImage v-if="blok" class="w-full" :image="blok.image" />
+  </div>
+</template>
+
+<script setup>
+  defineProps({
+    blok: {
+      type: Object,
+      required: true
+    }
+  });
+</script>
