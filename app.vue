11d47e64b16dc9e2353b71a9608f9d1960d07f2f
Özkan Yanikbas <oezkanyanikbas@gmail.com>
2022-06-03 20:58:55 +0200

    Add dynmaic pages

--- app.vue
+++ app.vue
@@ -1,5 +1,3 @@
 <template>
-  <div>
-    <NuxtWelcome />
-  </div>
+  <NuxtChild />
 </template>
---
+++ pages/[...uid].vue
@@ -0,0 +1,36 @@
+<template>
+  <!-- Layout -->
+  <NuxtLayout :name="layout">
+    <!-- SEO -->
+    <Head>
+      <Title>{{ story.content.seo_title }}</Title>
+      <Meta name="description" :content="story.content.seo_description" />
+    </Head>
+
+    <!-- Storyblok -->
+    <StoryblokComponent v-if="story" :blok="story.content" />
+  </NuxtLayout>
+</template>
+
+<script setup>
+  definePageMeta({
+    layout: false
+  });
+
+  const route = useRoute();
+
+  const layout = ref('default');
+  const story = ref(null);
+
+  // Fetch Story
+  const res = await useStoryblok(
+    `${route.fullPath === '/' ? '/home' : route.fullPath}`,
+    { version: 'draft' }
+  );
+  story.value = res.value;
+  layout.value = story.value ? story.value.content.layout : layout.value;
+
+  onMounted(() => {
+    useStoryblokBridge(story.value.id, (evStory) => (story.value = evStory));
+  });
+</script>
