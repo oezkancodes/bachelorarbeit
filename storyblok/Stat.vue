b0c0ae30e605a084d16e1b98f0a57ac3c02dc703
Özkan Yanikbas <oezkanyanikbas@gmail.com>
2022-06-03 21:50:34 +0200

    Add Stat Blok

---
+++ storyblok/Stat.vue
@@ -0,0 +1,21 @@
+<template>
+  <div v-if="blok" class="flex flex-col px-8 pt-8">
+    <dt
+      class="order-2 text-base font-medium text-gray-500"
+      v-text="blok.title"
+    />
+    <dd
+      class="order-1 text-2xl font-extrabold text-indigo-600 sm:text-3xl"
+      v-text="blok.value"
+    />
+  </div>
+</template>
+
+<script setup>
+  defineProps({
+    blok: {
+      type: Object,
+      required: true
+    }
+  });
+</script>
