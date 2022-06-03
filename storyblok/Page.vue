27a59f431021c269e6bda8decb9660360bdaa635
Özkan Yanikbas <oezkanyanikbas@gmail.com>
2022-06-03 20:18:13 +0200

    Add Page Blok

---
+++ storyblok/Page.vue
@@ -0,0 +1,18 @@
+<template>
+  <div v-if="blok" v-editable="blok">
+    <StoryblokComponent
+      v-for="each_blok in blok.block"
+      :key="each_blok._uid"
+      :blok="each_blok"
+    />
+  </div>
+</template>
+
+<script setup>
+  defineProps({
+    blok: {
+      type: Object,
+      required: true
+    }
+  });
+</script>
