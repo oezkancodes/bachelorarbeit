bb2e6e561db149f8159c07c2c3dbf786a1195639
Özkan Yanikbas <oezkanyanikbas@gmail.com>
2022-06-03 21:55:25 +0200

    Add Feature Blok

---
+++ storyblok/Feature.vue
@@ -0,0 +1,28 @@
+<template>
+  <div v-if="blok">
+    <div>
+      <span
+        class="flex h-12 w-12 items-center justify-center rounded-md bg-white bg-opacity-10"
+      >
+        <StoryblokImage class="h-6 w-6" :image="blok.icon" />
+      </span>
+    </div>
+    <div class="mt-6">
+      <h3 class="text-lg font-medium text-white" v-text="blok.title" />
+
+      <StoryblokRichText
+        class="mt-2 text-base text-indigo-200"
+        :text="blok.description"
+      />
+    </div>
+  </div>
+</template>
+
+<script setup>
+  defineProps({
+    blok: {
+      type: Object,
+      required: true
+    }
+  });
+</script>
