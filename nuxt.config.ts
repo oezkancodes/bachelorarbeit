8736593e844da1c34fdbab379ee6787eddf0e954
Özkan Yanikbas <oezkanyanikbas@gmail.com>
2022-06-05 00:47:06 +0200

    Change target & fallback

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -5,7 +5,7 @@ import { tailwindcss } from './config/nuxt.tailwind';
 
 // https://v3.nuxtjs.org/api/configuration/nuxt.config
 export default defineNuxtConfig({
-  target: 'server',
+  target: 'static',
 
   generate: {
     fallback: '404.html'
