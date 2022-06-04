1c9b5c7aadb8abcef15d87d50cd193b089f0f482
Özkan Yanikbas <oezkanyanikbas@gmail.com>
2022-06-05 00:06:32 +0200

    Rework dynamic pages in TS with Error Handling

--- pages/[...uid].vue
+++ pages/[...uid].vue
@@ -1,7 +1,7 @@
 <template>
-  <main>
+  <main v-if="story">
     <!-- Layout -->
-    <NuxtLayout :name="layout">
+    <NuxtLayout :name="story.content.layout || 'default'">
       <!-- SEO -->
       <Head>
         <Title>{{ seo.title }}</Title>
@@ -15,48 +15,55 @@
       </Head>
 
       <!-- Storyblok -->
-      <component
-        :is="story.content.component"
-        v-if="story"
-        :blok="story.content"
-      />
+      <component :is="story.content.component" :blok="story.content" />
     </NuxtLayout>
   </main>
 </template>
 
-<script setup>
+<script setup lang="ts">
+  import { Story, StoryData } from 'storyblok-js-client';
+  import { SEO } from '~/@types/SEO';
+
   definePageMeta({
     // https://v3.nuxtjs.org/guide/directory-structure/layouts#example-manual-control-with-pages
     layout: false
   });
 
+  // Utils
+  const storyblokApi = useStoryblokApi();
   const route = useRoute();
 
-  const story = ref(null);
-  const layout = computed(() =>
-    !story ? 'default' : story.value.content.layout
-  );
+  // State
+  const story = ref<StoryData>(null);
 
-  // Fetch Story
-  const res = await useStoryblok(realPathResolver(route.path), {
-    version: 'draft'
-  });
-  story.value = res.value;
+  // Fetch data
+  try {
+    const { data }: Story = await storyblokApi.get(
+      'cdn/stories/' + realPathResolver(route.path),
+      {
+        version: 'draft'
+      }
+    );
+    story.value = data.story;
+  } catch (err) {
+    throwError(err.response.status + ' ' + err.response.statusText);
+  }
 
   // SEO
-  const seo = {
-    title: story.value.content.seo_title,
-    description: story.value.content.seo_description,
-    image: story.value.content.seo_image.filename
+  const seo = ref<SEO>({
+    title: story.value?.content.seo_title,
+    description: story.value?.content.seo_description,
+    image: story.value?.content.seo_image.filename
       ? story.value.content.seo_image.filename
       : null,
     url: 'https://bachelorarbeit.netlify.app' + linkResolver(route.path),
     type: 'website',
     locale: 'de_DE'
-  };
+  });
 
   onMounted(() => {
     // Listen for changes from Storyblok visual editor
+    if (!story.value) return;
     useStoryblokBridge(story.value.id, (evStory) => (story.value = evStory));
   });
 </script>
