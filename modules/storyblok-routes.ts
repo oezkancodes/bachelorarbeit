ab6b76ae10ddf5d5c0dd53248b179463c96a67d4
Özkan Yanikbas <oezkanyanikbas@gmail.com>
2022-06-08 20:49:30 +0200

    Rework custom Storyblok module

--- modules/storyblok-routes.ts
+++ modules/storyblok-routes.ts
@@ -1,44 +1,58 @@
 import StoryblokClient, { StoryData } from 'storyblok-js-client';
 import { NitroConfig } from 'nitropack';
+import { defineNuxtModule } from '@nuxt/kit';
 import { linkResolver } from '../composables/storyblok';
 
-/**
- * Fetch routes from Storyblok API
- */
-export default async (_inlineOptions, nuxt) => {
-  await nuxt.hook('nitro:config', async (nitroConfig: NitroConfig) => {
-    if (nitroConfig.dev) {
-      return;
+// https://v3.nuxtjs.org/guide/going-further/modules#module-author-guide
+export default defineNuxtModule({
+  meta: {
+    name: 'nuxt-storyblok',
+    configKey: 'nuxt-storyblok',
+    compatibility: {
+      nuxt: '^3.0.0'
     }
+  },
+  defaults: {},
+  hooks: {
+    /**
+     * Fetch dynamic routes from Storyblok API
+     */
+    'nitro:config': async (nitroConfig: NitroConfig) => {
+      if (nitroConfig.dev) {
+        return;
+      }
 
-    // Fetch routes from Storyblok API
-    const storyblokApi = new StoryblokClient({
-      accessToken: process.env.STORYBLOK_ACCESS_TOKEN
-    });
-    const routes: string[] = await storyblokApi
-      .get('cdn/stories', {
-        starts_with: '',
-        /** Get routes in recursion when page size exceeds 100 */
-        per_page: 100,
-        excluding_slugs: '/configuration',
-        version: 'published'
-      })
-      .then((res) => {
-        let stories: StoryData[] = res.data.stories;
-        let routes: string[] = [];
-        // Filtering out stories that are not pages
-        stories = stories.filter(
-          (story) => !story.full_slug.includes('configuration/')
-        );
-        // Map stories to accessible paths
-        routes = stories.map((story) => '/' + linkResolver(story.full_slug));
-        console.log(routes);
-        return routes;
-      })
-      .catch((err) => {
-        console.log(err);
-        return [];
+      const storyblokApi = new StoryblokClient({
+        accessToken: process.env.STORYBLOK_ACCESS_TOKEN
       });
-    routes.forEach((path) => nitroConfig.prerender.routes.push(path));
-  });
-};
+
+      // Fetch routes from Storyblok API
+      const routes: string[] = await storyblokApi
+        .get('cdn/stories', {
+          starts_with: '',
+          // Get routes in recursion when page size exceeds 100
+          per_page: 100,
+          // excluding_routes seems to be ignored
+          excluding_slugs: '/configuration',
+          version: 'published'
+        })
+        .then((res) => {
+          let stories: StoryData[] = res.data.stories;
+          let routes: string[] = [];
+          // Filtering out stories that are not pages, as excluding_routes doesn't work
+          stories = stories.filter(
+            (story) => !story.full_slug.includes('configuration/')
+          );
+          // Map stories to accessible paths
+          routes = stories.map((story) => '/' + linkResolver(story.full_slug));
+          console.log(routes);
+          return routes;
+        })
+        .catch((err) => {
+          console.log(err);
+          return [];
+        });
+      routes.forEach((path) => nitroConfig.prerender.routes.push(path));
+    }
+  }
+});
