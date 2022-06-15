e5fb4741fa1ce21d257c7026807db04f5b8f2171
Özkan Yanikbas <oezkanyanikbas@gmail.com>
2022-06-15 23:51:39 +0200

    Add FooterSocialItem Blok

---
+++ components/FooterSocialItem.vue
@@ -0,0 +1,25 @@
+<template>
+  <a
+    v-if="blok && blok.link.cached_url"
+    :key="blok.link._uid"
+    v-editable="blok"
+    :href="blok.link.cached_url"
+    target="_blank"
+    class="text-gray-400 hover:text-gray-500"
+  >
+    <InstagramIcon v-if="blok.icon === 'instagram'" class="h-7 w-7" />
+    <GithubIcon v-if="blok.icon === 'github'" class="h-7 w-7" />
+  </a>
+</template>
+
+<script setup>
+  import InstagramIcon from '~/public/icons/instagram.svg?component';
+  import GithubIcon from '~/public/icons/github.svg?component';
+
+  defineProps({
+    blok: {
+      type: Object,
+      required: true
+    }
+  });
+</script>
