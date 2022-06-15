353f7a953f7730801caa95b0562a4a0c15467db0
Özkan Yanikbas <oezkanyanikbas@gmail.com>
2022-06-15 21:43:46 +0200

    Expand NavigationSubItem Blok

--- components/NavigationSubItem.vue
+++ components/NavigationSubItem.vue
@@ -1,18 +1,34 @@
 <template>
-  <NuxtLink
-    v-if="blok"
-    :to="url"
-    class="-m-4 flex items-start rounded-lg p-3 hover:bg-gray-50"
-  >
-    <StoryblokImage class="h-6 w-6 flex-shrink-0" :image="blok.icon" />
+  <div v-if="blok">
+    <NuxtLink
+      v-if="type === 'story'"
+      :to="url"
+      class="-m-4 flex items-start rounded-lg p-3 hover:bg-gray-50"
+    >
+      <StoryblokImage class="h-6 w-6 flex-shrink-0" :image="blok.icon" />
 
-    <div class="ml-4">
-      <p class="text-base font-medium text-gray-900">{{ blok.label }}</p>
-      <p class="mt-1 text-sm text-gray-500">
-        {{ blok.description }}
-      </p>
-    </div>
-  </NuxtLink>
+      <div class="ml-4">
+        <p class="text-base font-medium text-gray-900">{{ blok.label }}</p>
+        <p class="mt-1 text-sm text-gray-500">
+          {{ blok.description }}
+        </p>
+      </div>
+    </NuxtLink>
+    <a
+      v-if="type === 'url'"
+      :href="url"
+      class="-m-4 flex items-start rounded-lg p-3 hover:bg-gray-50"
+    >
+      <StoryblokImage class="h-6 w-6 flex-shrink-0" :image="blok.icon" />
+
+      <div class="ml-4">
+        <p class="text-base font-medium text-gray-900">{{ blok.label }}</p>
+        <p class="mt-1 text-sm text-gray-500">
+          {{ blok.description }}
+        </p>
+      </div>
+    </a>
+  </div>
 </template>
 
 <script setup>
@@ -22,9 +38,10 @@
       required: true
     }
   });
-
-  const url =
-    props.blok.link.linktype === 'story'
+  const type = ref(props.blok.link.linktype);
+  const url = ref(
+    type.value === 'story'
       ? '/' + linkResolver(props.blok.link.cached_url)
-      : props.blok.link.cached_url;
+      : props.blok.link.cached_url
+  );
 </script>
